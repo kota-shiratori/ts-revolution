4ae04247cdcbf54ffed487202ab3bfa591fec422
shiratori <shiratori@elplanning.co.jp>
2026-05-08 14:38:06 +0900

    ✨

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="">Todo</h1>
+      <ul className="">
+        <li className="">Todo1</li>
+        <li className="">Todo2</li>
+        <li className="">Todo3</li>
+      </ul>
+    </div>
+  );
+}
